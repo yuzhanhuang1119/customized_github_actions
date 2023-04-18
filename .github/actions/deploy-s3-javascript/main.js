8ec3eaac1f276b7df58588510317d746793f3490
yuzhanhuang1119 <joe@tsohue.com>
2023-04-18 16:08:11 +0800

    echo website url

--- .github/actions/deploy-s3-javascript/main.js
+++ .github/actions/deploy-s3-javascript/main.js
@@ -11,8 +11,9 @@ function run () {
     // 2) Upload files
     const s3Uri = `s3://${bucket}`
     exec.exec(`aws s3 sync ${distFolder} ${s3Uri} --region ${bucketRegion}`)
-    core.notice('Hello from my custom Javascript Action!')
 
+    const websiteUrl = `http://${bucket}.s3-website-${bucketRegion}.amazonaws.com`
+    core.setOutput('website-url', websiteUrl)
 }
 
 run();
